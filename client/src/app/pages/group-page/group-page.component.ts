056cef2a5d1c5007305991a8e751e47a9aa0b761
unknown <shaicokohn@gmail.com>
2023-08-25 16:29:31 -0300

    Add group post function

--- client/src/app/pages/group-page/group-page.component.ts
+++ client/src/app/pages/group-page/group-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { GroupService } from 'src/app/services/group.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-group-page',
@@ -15,6 +16,14 @@ export class GroupPageComponent {
   groupID:any = ""
   group:any = {}
 
+  postData = new FormGroup({
+    text: new FormControl(''),
+    username: new FormControl(''),
+    userID: new FormControl(''),
+    groupID: new FormControl(''),
+    photos: new FormControl(''),
+  })
+
   getGroup() {
     this.route.params.subscribe(params => {
       this.groupID = params });
@@ -24,6 +33,25 @@ export class GroupPageComponent {
         this.group = res
       },
       err => console.log(err)
+    )
+  }
+
+  addGroupPost() {
+    const user = JSON.parse(localStorage.getItem('user')!)
+    this.postData.value.userID = user.result._id
+    this.postData.value.groupID = this.groupID
+    this.postData.value.username = user.result.username
+    this.groupService.addPost(this.postData.value)
+    .subscribe(
+      res => {
+        console.log('res', res)
+      },
+      err => Swal.fire({
+        title: "Error",
+        text: err.error.message,
+        icon: "error",
+        timer: 2000,
+    })
     )
   }
 }
